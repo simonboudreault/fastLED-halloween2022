94fd1c6e9f817c9a144328cfe694c595a343aa27
Simon Boudreault <simonboudreault04@gmail.com>
2022-12-10 11:41:26 -0500

    s

--- CD77_Halloween_Fun_2019.ino
+++ CD77_Halloween_Fun_2019.ino
@@ -1,5 +1,5 @@
 /*
- 
+
 CD77_Halloween_Fun_2019 by Chemdoc77
 
 Based by code from different sources.
@@ -10,13 +10,12 @@ See each tab for the sources.
 #include <FastLED.h>
 
 #define LED_PIN 6
-#define CHIPSET NEOPIXEL
-#define NUM_LEDS 90
+#define CHIPSET WS2811
+#define NUM_LEDS 50
 
 #define BRIGHTNESS 200
 
-
-//Time Performance code
+// Time Performance code
 
 uint32_t gTimeCodeBase = 0;
 uint32_t gTimeCode = 0;
@@ -26,47 +25,43 @@ uint32_t gLastTimeCodeDoneFrom = 0;
 
 CRGB leds[NUM_LEDS];
 
-
-//Fire2012withPalette stuff
+// Fire2012withPalette stuff
 CRGBPalette16 gPal;
 #define FRAMES_PER_SECOND 60
 
 bool gReverseDirection = false;
 
-
-
 //========================
-
 
 #include "Halloween_chase.h"
 #include "Fire2012withPalette.h"
 #include "Flicker.h"
 #include "Time_performance.h"
 
-void setup() {
+void setup()
+{
 
-delay(1000); // sanity delay
-FastLED.addLeds<CHIPSET, LED_PIN>(leds, NUM_LEDS);
-FastLED.setBrightness( BRIGHTNESS );
+  delay(1000); // sanity delay
+  FastLED.addLeds<CHIPSET, LED_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 
-//Fire2012withPalette stuff
- gPal = HeatColors_p;
-  
+  // Fire2012withPalette stuff
+  gPal = HeatColors_p;
 
-FastLED.setMaxPowerInVoltsAndMilliamps(5,7500);
- set_max_power_indicator_LED(13);
- fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 7500);
+  set_max_power_indicator_LED(13);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
   FastLED.show();
-  
-//Time Performance code
-   RestartPerformance();
- 
+
+  // Time Performance code
+  RestartPerformance();
 }
 
 //============================================
-void loop() {  
+void loop()
+{
 
- //Time Performance code
-  gTimeCode = millis() - gTimeCodeBase;  
+  // Time Performance code
+  gTimeCode = millis() - gTimeCodeBase;
   Performance5();
 }
